4ce1408d5777e76cd07bb2bad491d6466e91234d
Benjamin Harris <benjamin.harris@digitalcareerinstitute.org>
2021-08-23 13:40:48 +0200

    added solution branch

---
+++ index.js
@@ -0,0 +1,37 @@
+// 1
+// function multiply(a, b) {
+//   console.log(a * b);
+// }
+
+// multiply(3, 4);
+
+// 2
+// const multiply = function(a, b) {
+//   console.log(a * b);
+// };
+
+// multiply(3, 4);
+
+
+// 3 
+// const multiply = (a, b) => console.log(a * b);
+// multiply(3, 4)
+
+// 4
+// function declaration
+// function remainderOfDivision(a, b) {
+//     console.log(a % b);
+// }
+
+// remainderOfDivision(6, 3);
+
+// function declaration as value
+// const remainderOfDivision = function(a, b) {
+//     console.log(a % b);
+// }
+
+// remainderOfDivision(6, 3);
+
+// function isEven(number) {
+//  return number % 2 === 0;
+// }
